9ff1f5a34e64f93b6bbd41a0da1b240b8e27c9a4
chotok <motoc.razvan@proton.me>
2026-03-11 09:53:48 +0200

    Modified timezone

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,11 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+     public function __construct($environment, $debug)
+    {
+        date_default_timezone_set( 'Europe/Bucharest' );
+        parent::__construct($environment, $debug);
+    }
 }
+
